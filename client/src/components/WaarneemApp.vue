fefad6ee7d87bdc62263f3d8ca78734f81db8ef8
mohit dutt <code3dworld@gmail.com>
2023-06-29 15:22:40 +0530

    Initial commit

---
+++ client/src/components/WaarneemApp.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="d-flex background">
+    <ShiftsLeft @updateSideBarData="updateSideBarData"/>
+    <DrawerRight :updateSideBar="updateSideBar"/>
+  </div>
+</template>
+
+<script>
+import "../style/WaarneemApp.css";
+import ShiftsLeft from "../components/ShiftsLeft.vue"
+import DrawerRight from "../components/DrawerRight.vue";
+export default {
+  name: 'WaarneemApp',
+  data(){
+    return {
+      updateSideBar: {
+        sideBar: false,
+        action: "add"
+      }
+    }
+  },
+  components: {
+    ShiftsLeft,
+    DrawerRight
+  },
+  methods: {
+    updateSideBarData(data){
+      this.updateSideBar.sideBar = data.sideBar;
+      this.updateSideBar.action = data.action;
+    }
+  }
+}
+</script>
